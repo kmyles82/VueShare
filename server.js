41099e9f1b6084f1e6afd104a270153d4a73ccfd
kmyles82 <kmyles82@gmail.com>
2020-09-10 21:09:46 -0500

    created signupUser mutation

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 //bring in apollo server
-const { ApolloServer, gql }  = require('apollo-server');
+const { ApolloServer }  = require('apollo-server');
 
 //bring in mongoose to make connection to db
 const mongoose = require('mongoose');
 
+const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, 'typeDefs.gql')
+const typeDefs = fs.readFileSync(filePath, 'utf-8')
+const resolvers = require('./resolvers')
+
 //grab variables from dotenv file
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config({ path: './variables.env' });
 
 const User = require('./models/User')
 const Post = require('./models/Post')
@@ -20,16 +27,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 mongoose.set('useCreateIndex', true)
 
-const typeDefs = gql`
-
-type Query {
-  dummy: String
-}
-
-`;
-
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   context: {
     User,
     Post
